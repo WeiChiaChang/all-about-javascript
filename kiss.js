5201e5845d8a0631358f397cfa99ea34db3ebd23
WesleyChang <ioh04019@gmail.com>
2017-10-03 18:26:30 +0800

    Create kiss.js

---
+++ kiss.js
@@ -0,0 +1,6 @@
+// Original source from https://juejin.im/post/59d2ebcbf265da06516729b8
+// Generate random color
+// 隨機產生色碼
+function getRandomColor () {
+  return `#${Math.random().toString(16).substr(2, 6)}`
+}
