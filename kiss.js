ff3a919dcc91e0b28125c6ee0c129298a2c523d3
WesleyChang <ioh04019@gmail.com>
2017-10-03 18:32:17 +0800

    Update kiss.js

--- kiss.js
+++ kiss.js
@@ -1,6 +1,13 @@
 // Original source from https://juejin.im/post/59d2ebcbf265da06516729b8
 // Generate random color
 // 隨機產生色碼
-function getRandomColor () {
+function randomColor () {
   return `#${Math.random().toString(16).substr(2, 6)}`
+}
+
+// Original source from https://juejin.im/post/59d2ebcbf265da06516729b8
+// Get minimum value from array
+// 取得 Array 中最小值
+function minArr (array) {
+  return Math.min.apply(null, array)
 }
